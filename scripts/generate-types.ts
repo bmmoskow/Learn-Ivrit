d8719046ad0e395c844fd9307376b196765bd5e9
Ben Moskowitz <gmmoskow@amath.washington.edu>
2026-03-19 19:41:37 -0700

    fixing project id

--- scripts/generate-types.ts
+++ scripts/generate-types.ts
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'fs';
 import { join } from 'path';
 
-const SUPABASE_PROJECT_ID = 'igqupnhtbulncgokwbhe';
+const SUPABASE_PROJECT_ID = process.env.VITE_SUPABASE_PROJECT_ID;
 const SUPABASE_ACCESS_TOKEN = process.env.SUPABASE_ACCESS_TOKEN;
 
 async function generateTypes() {
