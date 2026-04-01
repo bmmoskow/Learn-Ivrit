48bee83665b5754f97f4d82a6890e6fe865906cf
Ben Moskowitz <gmmoskow@amath.washington.edu>
2026-04-01 08:21:43 -0700

    fixing merge issues with migrations

--- supabase/migrations/20260317052833_f4194e61-1a8a-41d7-8a76-98facfb85ba0.sql
+++ supabase/migrations/20260317052833_f4194e61-1a8a-41d7-8a76-98facfb85ba0.sql
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-
 -- Clear all existing logs (old data has no pricing_id anyway)
 TRUNCATE TABLE public.api_usage_logs;
 
 -- Drop the estimated_cost_usd column - costs will be calculated by joining api_pricing
 ALTER TABLE public.api_usage_logs DROP COLUMN IF EXISTS estimated_cost_usd;
-=======
-
--- Clear all existing logs (old data has no pricing_id anyway)
-TRUNCATE TABLE public.api_usage_logs;
-
--- Drop the estimated_cost_usd column - costs will be calculated by joining api_pricing
-ALTER TABLE public.api_usage_logs DROP COLUMN IF EXISTS estimated_cost_usd;
->>>>>>> b7894df8078cd44dd9b5d2b90c24436d3b548327
